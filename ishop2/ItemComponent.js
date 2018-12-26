e88fca4d903ee1ac57e0195a570459db5992432d
Yuliya < >
2018-12-26 22:57:28 +0300

    'first_commit'

---
+++ ishop2/ItemComponent.js
@@ -0,0 +1,40 @@
+var ItemComponent = React.createClass({
+    displayName:'ItemComponent',
+    propTypes:{
+        code: React.PropTypes.string.isRequired,
+        type: React.PropTypes.string.isRequired,
+        brand: React.PropTypes.string.isRequired,
+        model: React.PropTypes.string.isRequired,
+        power: React.PropTypes.number.isRequired,
+        photoURL: React.PropTypes.any.isRequired,
+        quantity: React.PropTypes.number.isRequired,
+        price: React.PropTypes.number.isRequired,
+        isSelected: React.PropTypes.bool,
+        cbSelectedItemCode: React.PropTypes.func.isRequired,
+        cbDelete: React.PropTypes.func.isRequired
+    },
+    selectedItemCode: function(){
+        this.props.cbSelectedItemCode(this.props.code);
+    },
+    delete: function(){
+        this.props.cbDelete(this.props.code);
+    },
+    render: function() {
+        return React.DOM.div({
+                className: 'Article',
+                onClick:this.selectedItemCode,
+                style:this.props.isSelected?({backgroundColor:'#ff7f50'}):null
+            },
+            React.DOM.div({className:'Type'},this.props.type),
+            React.DOM.div({className:'Brand'},this.props.brand),
+            React.DOM.div({className:'Model'},this.props.model),
+            React.DOM.div({className:'Power'},this.props.power),
+            React.DOM.div({className:'Photo'},this.props.photoURL),
+            React.DOM.div({className:'Quantity'},this.props.quantity),
+            React.DOM.div({className:'Price'},this.props.price),
+            React.DOM.div({className:'Delete'},
+                React.DOM.input({type:'button',value:'Delete',onClick:this.delete})
+            ),
+        )
+    },
+})
---
+++ ishop2/ListComponent.js
@@ -0,0 +1,59 @@
+var ListComponent = React.createClass({
+    displayName:'ListComponent',
+    propTypes: {
+        name: React.PropTypes.string.isRequired,
+        head: React.PropTypes.arrayOf(React.PropTypes.string.isRequired),
+        articles: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                code: React.PropTypes.string.isRequired,
+                type: React.PropTypes.string.isRequired,
+                brand: React.PropTypes.string.isRequired,
+                model: React.PropTypes.string.isRequired,
+                power: React.PropTypes.number.isRequired,
+                photoURL: React.PropTypes.any.isRequired,
+                quantity: React.PropTypes.number.isRequired,
+                price: React.PropTypes.number.isRequired
+            })
+        )
+    },
+    getInitialState: function(){
+        return{
+            selectedItem: null,
+            itemsList: this.props.articles,
+        }
+    },
+    selectedItemCode: function(code){
+        this.setState({selectedItem:code});
+    },
+    delete: function(code){
+        confirm('Вы действительно хотите удалить этот товар?')?
+            (this.setState({itemsList: this.state.itemsList.filter(item => item.code!= code),})
+            ):null;
+    },
+    render: function(){
+        var head = this.props.head.map(title =>
+            React.DOM.div({key: Math.random(),className:'Title'}, title)
+    );
+        var items = this.state.itemsList.map(item =>
+            React.createElement(ItemComponent,{
+                key:item.code,
+                code:item.code,
+                type:item.type,
+                brand:item.brand,
+                model:item.model,
+                power:item.power,
+                photoURL:item.photoURL,
+                quantity:item.quantity,
+                price:item.price,
+                isSelected:(this.state.selectedItem==item.code),
+                cbSelectedItemCode: this.selectedItemCode,
+                cbDelete: this.delete
+            })
+    );
+        return React.DOM.div({className:'Catalogue'},
+            React.DOM.div({className:'Text'}, this.props.name),
+            React.DOM.div({className:'Titles'}, head),
+            React.DOM.div({className:'ListContent'}, items),
+        );
+    },
+});
